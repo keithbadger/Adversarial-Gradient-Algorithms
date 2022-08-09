617a32fe02a54aef80b8b3039e6cf90b4cb36990
keithbadger <106630519+keithbadger@users.noreply.github.com>
2022-08-09 14:45:17 -0400

    Add files via upload

--- Conditional.py
+++ Conditional.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.optimize as opt
 
-index = 1 # index of given grid size
+index = 3 # index of given grid size
 n = 8 # n is the number of rows and columns
 l = 1000 # l is the number of iterations
 alpha = .01 #stepsize parameter
@@ -12,18 +11,28 @@ x = np.random.rand(n,1) # is the probability of selecting a row with maximizatio
 y = np.random.rand(n,1) # is the probability of selecting a col with minimization goal
 x = x / x.sum()
 y = y / y.sum()
+x_sum = np.zeros((n,1))
+y_sum = np.zeros((n,1))
 r_t = np.array([]) # record of rewards
 u_t = np.array([]) # record of upperbounds
 l_t = np.array([]) # record of lowerbounds
+"""-----------------
+x = np.array([[.999],[0.0005],[0.0005]])
+y = np.array([[0.0005],[.999],[0.0005]])
+x0 = np.array([])
+x1 = np.array([])
+x2 = np.array([])
+y0 = np.array([])
+y1 = np.array([])
+y2 = np.array([])
+#"""
 game_file = open(str(n)+'x'+str(n)+ " #" + str(index), 'r')
+#game_file = open("RPS")
 G = np.loadtxt(game_file)
 game_file.close()
 
-for i in range(l):
-    alpha = scale/(scale+i)
-    r_t = np.append(r_t, x.transpose() @ G @ y)
-    u_t = np.append(u_t,((opt.linprog(-G@y , A_eq=np.ones((1,n)), b_eq=[1])).x@G@y)[0])
-    l_t = np.append(l_t,x.T@G@np.array([(opt.linprog(G.T@x, A_eq=np.ones((1,n)), b_eq=[1])).x]).T)
+for k in range(l):
+    #alpha = scale/(scale+i)
     tmp = x.copy()
     xbar = np.zeros((n,1))
     xbar[np.argmax(G @ y)] = 1
@@ -31,31 +40,55 @@ for i in range(l):
     ybar[np.argmin(G.transpose() @ tmp)] = 1
     x = x + alpha*(xbar - x)
     y = y + alpha*(ybar - y)
+    x_sum = x_sum + x
+    y_sum = y_sum + y
+    xa = x_sum/(k+1)
+    ya = y_sum/(k+1)
+    r_t = np.append(r_t, xa.transpose() @ G @ ya)
+    u_t = np.append(u_t,np.max(G@ya))
+    l_t = np.append(l_t,np.min(G.T@xa))
+    """-----------------
+    x0 = np.append(x0, x[0])
+    x1 = np.append(x1, x[1])
+    x2 = np.append(x2, x[2])
+    y0 = np.append(y0, y[0])
+    y1 = np.append(y1, y[1])
+    y2 = np.append(y2, y[2])
+    #"""
+upper = np.max(G@ya)
+lower = np.min(G.T@xa)
 
-upper = opt.linprog(-G@y , A_eq=np.ones((1,n)), b_eq=[1])
-lower = opt.linprog(G.T@x, A_eq=np.ones((1,n)), b_eq=[1])
-
-
-#"""
+#"""-------------------
 print("+===============+")
 print("|  CONDITIONAL  |")
 print("+===============+")
 print("| ROW   |   COL |")
 print("+===============+")
 for p in range(n):
-    print(f"| {x[p][0]:.2f}  |  {y[p][0]:.2f} |")
+    print(f"| {xa[p][0]:.2f}  |  {ya[p][0]:.2f} |")
 print("+===============+")
-print(f"|REWARD: {(x.T@G@y)[0][0]:7.2f}|")
-print(f"|UPPER:  {([upper.x]@G@y)[0][0]:7.2f}|")
-print(f"|LOWER:  {(x.T@G@np.array([lower.x]).T)[0][0]:7.2f}|")
+print(f"|REWARD: {(xa.T@G@ya)[0][0]:7.2f}|")
+print(f"|UPPER:  {upper:7.2f}|")
+print(f"|LOWER:  {lower:7.2f}|")
 print("+===============+")
-print(f"|GAP:    {([upper.x]@G@y)[0][0] - (x.T@G@np.array([lower.x]).T)[0][0]:7.2f}|")
+print(f"|GAP:    {upper - lower:7.2f}|")
 print("+===============+")
 #"""
-
+#"""------------------
 t = np.array([list(range(l))])
 plt.plot(t.T,r_t,'b',t.T,u_t,'g',t.T,l_t,'r')
 plt.xlabel("Iteration")
 plt.ylabel("Reward")
 plt.legend(["Reward","Upperbound","Lowerbound"])
 plt.show()
+#"""
+"""------------------
+ax = plt.axes(projection='3d')
+ax.plot3D(np.array([1,0,0,1]),np.array([0,1,0,0]),np.array([0,0,1,0]),'black')
+ax.plot3D(x0,x1,x2,'green')
+ax.plot3D(y0,y1,y2,'red')
+ax.set_xlabel("rock")
+ax.set_ylabel("paper")
+ax.set_zlabel("scissors")
+plt.show()
+#"""
--- KL_Penalized.py
+++ KL_Penalized.py
@@ -1,10 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.optimize as opt
 
-index = 1  # index of given grid size
+index = 3  # index of given grid size
 n = 8 # n is the number of rows and columns
-l = 1000 # l is the number of iterations
+l = 10000 # l is the number of iterations
 alpha = .01 #stepsize parameter
 e = 1 # penalty coeff
 scale = 5
@@ -13,45 +12,69 @@ x = np.random.rand(n,1) # is the probability of selecting a row with maximizatio
 y = np.random.rand(n,1) # is the probability of selecting a col with minimization goal
 x = x / x.sum()
 y = y / y.sum()
+"""------------------
+x = np.array([[.999],[0.0005],[0.0005]])
+y = np.array([[0.0005],[.999],[0.0005]])
+x0 = np.array([])
+x1 = np.array([])
+x2 = np.array([])
+y0 = np.array([])
+y1 = np.array([])
+y2 = np.array([])
+#"""
+x_sum = np.zeros((n,1))
+y_sum = np.zeros((n,1))
 r_t = np.array([]) # record of rewards
 u_t = np.array([]) # record of upperbounds
 l_t = np.array([]) # record of lowerbounds
 game_file = open(str(n)+'x'+str(n)+ " #" + str(index), 'r')
+#game_file = open("RPS")
 G = np.loadtxt(game_file)
 game_file.close()
 
 for k in range(l):
-    alpha = scale/(scale + k)
-    r_t = np.append(r_t, x.transpose() @ G @ y)
-    u_t = np.append(u_t,((opt.linprog(-G@y , A_eq=np.ones((1,n)), b_eq=[1])).x@G@y)[0])
-    l_t = np.append(l_t,x.T@G@np.array([(opt.linprog(G.T@x, A_eq=np.ones((1,n)), b_eq=[1])).x]).T)
+    #alpha = scale/(scale + k)
     xbar = x * np.exp(G @ y / e)
     xbar = xbar/xbar.sum()
     ybar = y * np.exp(-G.T @ x / e)
     ybar = ybar/ybar.sum()
     x = x + alpha*(xbar - x)
     y = y + alpha*(ybar - y)
+    x_sum = x_sum + x
+    y_sum = y_sum + y
+    xa = x_sum/(k+1)
+    ya = y_sum/(k+1)
+    r_t = np.append(r_t, xa.transpose() @ G @ ya)
+    u_t = np.append(u_t,np.max(G@ya))
+    l_t = np.append(l_t,np.min(G.T@xa))
+    """--------------
+    x0 = np.append(x0, x[0])
+    x1 = np.append(x1, x[1])
+    x2 = np.append(x2, x[2])
+    y0 = np.append(y0, y[0])
+    y1 = np.append(y1, y[1])
+    y2 = np.append(y2, y[2])
+    #"""
 
-upper = opt.linprog(-G@y , A_eq=np.ones((1,n)), b_eq=[1])
-lower = opt.linprog(G.T@x, A_eq=np.ones((1,n)), b_eq=[1])
+upper = np.max(G@ya)
+lower = np.min(G.T@xa)
 
-#"""
+#"""-------------
 print("+===============+")
 print("|    SOFTMAX    |")
 print("+===============+")
 print("| ROW   |   COL |")
 print("+===============+")
 for p in range(n):
-    print(f"| {x[p][0]:.2f}  |  {y[p][0]:.2f} |")
+    print(f"| {xa[p][0]:.2f}  |  {ya[p][0]:.2f} |")
 print("+===============+")
-print(f"|REWARD: {(x.T@G@y)[0][0]:7.2f}|")
-print(f"|UPPER:  {([upper.x]@G@y)[0][0]:7.2f}|")
-print(f"|LOWER:  {(x.T@G@np.array([lower.x]).T)[0][0]:7.2f}|")
+print(f"|REWARD: {(xa.T@G@ya)[0][0]:7.2f}|")
+print(f"|UPPER:  {upper:7.2f}|")
+print(f"|LOWER:  {lower:7.2f}|")
 print("+===============+")
-print(f"|GAP:    {([upper.x]@G@y)[0][0] - (x.T@G@np.array([lower.x]).T)[0][0]:7.2f}|")
+print(f"|GAP:    {upper - lower:7.2f}|")
 print("+===============+")
 #"""
-
 
 
 t = np.array([list(range(l))])
@@ -60,3 +83,13 @@ plt.xlabel("Iteration")
 plt.ylabel("Reward")
 plt.legend(["Reward","Upperbound","Lowerbound"])
 plt.show()
+"""-------------
+ax = plt.axes(projection='3d')
+ax.plot3D(np.array([1,0,0,1]),np.array([0,1,0,0]),np.array([0,0,1,0]),'black')
+ax.plot3D(x0,x1,x2,'green')
+ax.plot3D(y0,y1,y2,'red')
+ax.set_xlabel("rock")
+ax.set_ylabel("paper")
+ax.set_zlabel("scissors")
+plt.show()
+#"""
